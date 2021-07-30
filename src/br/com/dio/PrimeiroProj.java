acd570582c474d4470acfa1fd10f176dffc6e329
Fabio Ferreira Reis <machshev.reis@gmail.com>
2021-07-29 22:25:13 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProj.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class PrimeiroProj {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        System.out.println("Projeto"  + (a+b));
+    }
+}
